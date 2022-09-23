3379cec7811217807e1c46164a4b342707eb30d9
KouRo <koushi.rockv@gmail.com>
2022-09-24 04:43:19 +0900

    Add a function to check if it is installed

---
+++ Build.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtCoderAutomationTool
+{
+    internal class Build
+    {
+        internal static void InstalledCheck(string fileName, string arguments,bool allowInstall)
+        {
+            using (var pro = new Process())
+            {
+                var psi = new ProcessStartInfo("cmd.exe", arguments);
+                psi.CreateNoWindow = true;
+                psi.UseShellExecute = false;
+                psi.RedirectStandardError = true;
+                psi.RedirectStandardInput = true;
+                psi.RedirectStandardInput = true;
+                pro.StartInfo = psi;
+                pro.Start();
+                pro.WaitForExit();
+                StreamReader sr = pro.StandardError;
+                string errorSentence=sr.ReadToEnd();
+                if (errorSentence.Length>0)
+                {
+                    Console.WriteLine("error :\n"+errorSentence);
+
+                }
+                else
+                {
+                    Console.WriteLine(fileName+" :OK");
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace AtCoderAutomationTool
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-
+            while(true)
+            {
+                Console.Write("Command :");
+                string[] commands = Console.ReadLine().Split();
+                if (commands[0]=="build")
+                {
+                    Build.InstalledCheck("npm", "/c npm -v",false);
+                    Build.InstalledCheck("pip3", "/c pip3 -v", false);
+                }
+                else if (commands[0] == "quit") break;
+                else Console.Error.WriteLine("Not found the command :"+commands[0]);
+                Console.WriteLine();
+            }
         }
     }
 }
