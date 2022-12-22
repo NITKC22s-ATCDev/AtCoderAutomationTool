f9ac3537eccb75b02ca16da2aaedbc1475e73468
Kou-Ro <koushi.rockv@gmail.com>
2022-12-22 20:57:23 +0900

    login

--- Commander.cs
+++ Commander.cs
@@ -12,6 +12,13 @@
             {
                Build.Run();
             }
+            else if (commands[0] == "login")
+            {
+               if (commands[1] == "acc") Login.Acc();
+               else if (commands[1] == "oj") Login.Oj();
+               else if (commands[1]=="") Console.Error.WriteLine("Please enter some option");
+               else Console.Error.WriteLine("The option not found :" + commands[1]);
+            }
             else if (commands[0] == "quit") break;
             else if (commands[0] == "") Console.Error.WriteLine("Please enter some command.");
             else Console.Error.WriteLine("The command not found :" + commands[0]);
--- Login.cs
+++ Login.cs
@@ -4,52 +4,14 @@ namespace AtCoderAutomationTool
 {
    internal class Login
    {
-      internal static void Control()
+      internal static void Acc()
       {
-         Console.Write("User Name :");
-         string userName = Console.ReadLine();
-
-         Console.Write("Password :");
-         Console.ForegroundColor = ConsoleColor.DarkGray;
-         Console.Write("(Hidden)");
-         Console.ResetColor();
-         string password = null;
-         while (true)
-         {
-            ConsoleKeyInfo inputedkey = Console.ReadKey(true);
-            if (inputedkey.Key == ConsoleKey.Enter) break;
-            password += inputedkey.KeyChar;
-         }
-         Console.WriteLine();
-         Run("/c acc login", userName, password);
-         Console.WriteLine();
+         CommandRunner.Run("acc login");
       }
 
-      private static bool Run(string command, string userName, string password)
+      internal static void Oj()
       {
-         using (Process pro = new Process())
-         {
-            var psi = new ProcessStartInfo("cmd.exe", command);
-            psi.CreateNoWindow = false;
-            psi.UseShellExecute = false;
-            psi.RedirectStandardError = false;
-            psi.RedirectStandardOutput = false;
-            psi.RedirectStandardInput = true;
-
-            pro.StartInfo = psi;
-            pro.Start();
-
-            StreamWriter sw = pro.StandardInput;
-
-            sw.WriteLine(userName);
-            pro.WaitForExit();
-            sw.WriteLine(password);
-            pro.WaitForExit();
-
-
-            Console.WriteLine();
-            return true;
-         }
+         CommandRunner.Run("oj login https://atcoder.jp");
       }
 
    }
