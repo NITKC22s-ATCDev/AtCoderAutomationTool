eff13b211d4c774a63b8e4e06c95302075acc51f
Kou-Ro <koushi.rockv@gmail.com>
2023-02-04 23:33:13 +0900

    Sentence correction

--- Commander.cs
+++ Commander.cs
@@ -36,11 +36,11 @@
                      else if (logoutYN == "n" || logoutYN == "N") break;
                   }
                }
-               else if (commands[1] == "") Console.Error.WriteLine("Please enter some option.");
+               else if (commands[1] == "") Console.Error.WriteLine("Please enter some options.");
                else Console.Error.WriteLine("The option not found :" + commands[1]);
             }
             else if (commands[0] == "quit") break;
-            else if (commands[0] == "") Console.Error.WriteLine("Please enter some command.");
+            else if (commands[0] == "") Console.Error.WriteLine("Please enter some commands.");
             else Console.Error.WriteLine("The command not found :" + commands[0]);
             Console.WriteLine();
          }
